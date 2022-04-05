73ddbdee1fb058be4602a3bd2379ac29bbf125e2
Sebasht <98536590+Sebasht@users.noreply.github.com>
2022-04-05 12:22:10 -0500

    Update Slots.cpp

--- parcial/Model/Slots.cpp
+++ parcial/Model/Slots.cpp
@@ -35,5 +35,5 @@ float Slots::calcularResultado(float gonzosApostar) {
 
 void Slots::mostrarReglas() {
     cout << "Slots es un juego en el que una maquina escoge aleatoriamente 3 simbolos (en algunos modelos son 5 Si caen los 3 simbolos iguales o alguna combinación especial el jugador ganará cierta cantidad, de lo contrario perdera lo apostado. \n";
-    cout << " \n";
+    cout << "En esta versión de Slots se generaran 3 numeros entre 1 y 7. Si caen los 3 numeros iguales el jugador ganará el doble de lo apostado, si consigue una escalera (numeros sucesivos sea ascendente o descendente) ganará la mitad de lo apostado. Si llega a conseguir el triple 7 ganará 7 veces lo apostado. Si no consigue ninguna perderá  lo apostado.\n";
 }
